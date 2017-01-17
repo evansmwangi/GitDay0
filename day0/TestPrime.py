68709af8c79e2cf0d9b2de0f4e540c0ec6d7fc61
evansmwangi <mwangievans01@gmail.com>
2017-01-17 15:26:56 +0300

    Tescase for prime numbers

---
+++ day0/TestPrime.py
@@ -0,0 +1,21 @@
+#Test case
+import unittest
+from primee import primee
+class PrimeeTestCases(unittest.TestCase):
+    def test_integer(self):
+        with self.assertRaises(TypeError):
+         prime-numbers ("String")
+
+     def test_list(self):
+          with self.assertRaises(TypeError):
+           prime-numbers ("list")
+    
+    def test_no_negative_numbers(self):
+      self.assertEqual(result, 'Invalid input', msg='Input below 0 not allowed')
+
+    def test_it_does_not_accept_float(self):
+       with self.assertRaises(ValueError) 
+    
+  def test_it_return_an_error_on_dict_input
+        with self.assertRaises(TypeError):
+
