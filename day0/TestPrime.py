5704671f7acb955b99d7f0ad9d98592552fee235
evansmwangi <mwangievans01@gmail.com>
2017-01-17 18:24:48 +0300

    testcase for prime numbers

--- day0/TestPrime.py
+++ day0/TestPrime.py
@@ -19,3 +19,16 @@ class PrimeeTestCases(unittest.TestCase):
   def test_it_return_an_error_on_dict_input
         with self.assertRaises(TypeError):
 
+    def test_divisible_by_2(self):
+        self.assertEqual(primee(2), 'divisible', msg='it is not prime')
+
+    def test_less_than_100(self):
+        self.assertEqual(primee(100), 'less', msg='the prime numbers upto 100')
+    def test_divisible_by_3(self):
+        self.assertEqual(primee(3), 'divisible', msg='it is not prime')
+
+   def test_input_integer_0(self):
+           self.assertEqual(primee(0), 'divisible', msg='it is  prime')
+  def   test_less_than_1000(self):
+        self.assertEqual(primee(1000), 'less', msg='the prime numbers upto 1000')
+  
