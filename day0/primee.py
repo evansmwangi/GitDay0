51519d67fb22579d77bbb2113c9168ac5e46e0b2
evansmwangi <mwangievans01@gmail.com>
2017-01-17 13:52:37 +0300

    day0 prime numbers function

---
+++ day0/primee.py
@@ -0,0 +1,10 @@
+def prime_number(n):
+	n=int(n)
+	for x in range (2,n+1):
+		isPrime=True
+		for y in range (2,int(x**0.5)+1):
+			if x%y==0:
+				isPrime==False
+				break
+			if isPrime==True
+				print(x)
