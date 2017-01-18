9a0b5085339ffd40c61e430e7aba400764030cd5
evansmwangi <mwangievans01@gmail.com>
2017-01-18 17:53:44 +0300

    Labs:day 2 labs

---
+++ Day2labs/MaxMin.py
@@ -0,0 +1,7 @@
+num_array =list[]
+number=raw_input('enter how many elements you want :')
+print ('enter number in array:')
+for i in range (int(number)):
+  n=raw_input ("number:")
+  num_array.append(int(number))
+  return ( min(num_array), max(num_array))
---
+++ Day2labs/wordcount.py
@@ -0,0 +1,10 @@
+def wordcount (words)
+names={}
+for word in word.split():
+  if names.get(words):
+    names[word] +=1
+    else:
+      names[word]=1
+    return names
+      
+
