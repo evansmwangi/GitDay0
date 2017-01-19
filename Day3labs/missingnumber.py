e64877981e11a26a4d941baeed8a40e3e4b6cf5c
evansmwangi <mwangievans01@gmail.com>
2017-01-19 16:34:27 +0300

    Missing Number Lab

---
+++ Day3labs/missingnumber.py
@@ -0,0 +1,13 @@
+def list1():
+    def list2():
+        def list3():
+            def list4():
+                list1-[1,2,3]
+                list2=[1,2,3,4]
+                list3=[4,66,7]
+                list4=[66,77,7,4]
+                diff1=list(set(list1)-set(list2))
+                diff2=list(set(list3)-set(list4))
+                return diff1
+                return diff2
+
